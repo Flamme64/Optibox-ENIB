ce51dbcb769ff1564cac7a618923a17ee606ed37
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-15 00:02:55 +0200

    Update point.h

--- point.h
+++ point.h
@@ -2,24 +2,26 @@
 #define POINT_H
 
 class Point{
-  //Attributs
   private :
-  int _x,_y,_z;
-  
-  //Méthodes génériques :
-  
-  //Constructeur
-  Point(int x, int y, int z) : _x(x), _y(y), _z(z) {}
-  //Destructeur
-  ~Point(void){}
-  //Accesseurs
-  int getX(void) {return _x;}
-  int getY(void) {return _y;}
-  int getZ(void) {return _z;}
-  //Mutateurs
-  void setX(int x) {_x=x;}
-  void setY(int y) {_y=y;}
-  void setZ(int z) {_z=z;}
+    //Attributs
+      int _x,_y,_z;
+      
+  public :
+    //Méthodes génériques :
+      //Constructeur
+      Point(int x, int y, int z) : _x(x), _y(y), _z(z) {}
+      //Destructeur
+      ~Point(void){}
+      
+      //Accesseurs
+      int getX(void) {return _x;}
+      int getY(void) {return _y;}
+      int getZ(void) {return _z;}
+      
+      //Mutateurs
+      void setX(int x) {_x=x;}
+      void setY(int y) {_y=y;}
+      void setZ(int z) {_z=z;}
   
 };
 
