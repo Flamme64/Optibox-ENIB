09521e37140e30e315e3316f760141897f1a4b43
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-15 00:53:09 +0200

    Update carton.cpp
    Il faudrait implémenter l'ordonnancement des dimensions

--- src/carton.cpp
+++ src/carton.cpp
@@ -2,8 +2,8 @@
 
 Carton::Carton(int x, int y, int z, int dimX, int dimY, int dimZ, const bool fragile, const float masse){
   
-  //Ordonnancement des coordonnées x,y,z pour les ranger dans un ordre décroissant
-  if(x>=y)
+  //Ordonnancement des dimensions dimX, dimY, dimZ pour les ranger dans un ordre décroissant
+  //A FAIRE
   
   //Création de l'objet
   Boite(x,y,z,dimX,dimY,dimZ);
