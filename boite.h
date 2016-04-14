8e8f57c7e0bd55e4ff9d5ad892c21e5e581f7110
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-15 00:18:09 +0200

    Update boite.h

--- boite.h
+++ boite.h
@@ -1,2 +1,27 @@
 #ifndef BOITE_H
-#define 
+#define BOITE_H
+
+#include "point.h"
+
+class Boite{
+  private :
+    //Attributs :
+      const int _dimX, _dimY, _dimZ;
+    
+  public :
+    //Attributs importés :
+      Point _point;
+    
+    //Méthodes génériques :
+      //Constructeur :
+      Boite(x,y,z,dimX,dimY,dimZ) : _dimX(dimX), _dimY(dimY), _dimZ(dimZ) {_point = Point(x,y,z);}
+      
+      //Accesseurs :
+      const int getDimX(void) {return _dimX;}
+      const int getDimY(void) {return _dimY;}
+      const int getDimZ(void) {return _dimZ;}
+  
+  
+};
+
+#endif
