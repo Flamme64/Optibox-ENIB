afca16cb3219611b7490f24a2c259fdceaf95bb8
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-15 01:22:51 +0200

    Update carton.cpp

--- src/carton.cpp
+++ src/carton.cpp
@@ -1,18 +1,35 @@
 #include "carton.h"
 
-Carton::Carton(int x, int y, int z, int dimX, int dimY, int dimZ, const bool fragile, const float masse){
+Carton::Carton(int x, int y, int z, const int dimX, const int dimY, const int dimZ, const bool fragile, const float masse){
   
-  //Ordonnancement des dimensions dimX, dimY, dimZ pour les ranger dans un ordre décroissant
-  //A FAIRE
+  // Rangement des dimensions par ordre décroissant
+  int cache;
+  for(int i=0; i<3; i++){
+    if(dimX < dimY){ //Si dimY est plus grande que dimX on met dimY dans dimX
+      cache = dimX;
+      dimX = dimY;
+      dimY = cache;
+    }
+    if(dimY < dimZ){ //Si dimZ est plus grande que dimY on met dimZ dans dimY
+      cache = dimY;
+      dimY = dimZ;
+      dimZ = cache;
+    }
+  }
   
-  //Création de l'objet
+  // Création de l'objet
   Boite(x,y,z,dimX,dimY,dimZ);
   _poidRang=NULL;
   _fragile=fragile;
-   _masse=masse;
-   
-  
-  
-  }
+  _masse=masse;
+  _maxDim=dimX; 
   
 }
+
+/* IDEES/REMARQUES :
+
+1 - Jacques
+  Est ce que maxDim a toujours un sens si on range les dimensions par ordre croissant ?
+
+
+*/
