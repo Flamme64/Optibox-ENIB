c564b3120e1a278181f701ffec80e8aa7d46e23e
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-15 12:29:45 +0200

    Update rangement.h

--- include/rangement.h
+++ include/rangement.h
@@ -1,6 +1,23 @@
 #ifndef RANGEMENT_H
 #define RANGEMENT
 
-class Rangement
+#include <vector>
+
+#include "contenant.h"
+#include "carton.h"
+
+class Rangement{
+  public :
+  // Attributs :
+    std::vector<Carton*> _cartRang;
+    std::vector<Contenant*> _contRang;
+  
+  // Méthodes génériques :
+  // Constructeur
+  Rangement(void) {_cartRang=NULL; _contRang=NULL;}
+  // Destructeur
+  ~Rangement(void) {}
+  
+};
 
 #endif
