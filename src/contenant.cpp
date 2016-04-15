3c289794c6066631a2d885507ce0513ea2620e19
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-15 12:18:35 +0200

    Update contenant.cpp

--- src/contenant.cpp
+++ src/contenant.cpp
@@ -1,7 +1,7 @@
 #include "contenant.h"
 
-Contenant::Contenant(int x=0, int y=0, int z=0, const int dimX, const  int dimY, const int dimZ, float chargeMax){
-  Boite(x,y,z,dimX,dimY,dimZ);
+Contenant::Contenant(int x=0, int y=0, int z=0, const int dimX, const  int dimY, const int dimZ, const std::string name, float chargeMax){
+  Boite(x,y,z,dimX,dimY,dimZ, name);
   _chargeMax=chargeMax;
   _cartons=NULL;
 }
