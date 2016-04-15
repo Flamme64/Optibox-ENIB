75f9c5393643a4a59b3f9da17ed17801add5530d
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-15 12:18:53 +0200

    Update carton.cpp

--- src/carton.cpp
+++ src/carton.cpp
@@ -1,6 +1,6 @@
 #include "carton.h"
 
-Carton::Carton(int x, int y, int z, const int dimX, const int dimY, const int dimZ, const bool fragile, const float masse){
+Carton::Carton(int x, int y, int z, const int dimX, const int dimY, const int dimZ, const std::string name, const bool fragile, const float masse){
   
   // Rangement des dimensions par ordre décroissant
   int cache;
@@ -18,7 +18,7 @@ Carton::Carton(int x, int y, int z, const int dimX, const int dimY, const int di
   }
   
   // Création de l'objet
-  Boite(x,y,z,dimX,dimY,dimZ);
+  Boite(x,y,z,dimX,dimY,dimZ,name);
   _poidRang=NULL; //Le poid de l'objet dans le tableau sera calculé quand on aura trouvé la méthode pour le faire
   _fragile=fragile;
   _masse=masse;
