cdf51a724c0d9bb6cf2570ba7d57eae54a7056bd
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-15 04:01:27 +0200

    Update contenant.h

--- include/contenant.h
+++ include/contenant.h
@@ -1,12 +1,33 @@
 #ifndef CONTENANT_H
 #define CONTENANT_H
 
+#include <vector>
+
 #include "boite.h"
+Class Carton; //Il faudra mettre un include dans le fichier final
 
 class Contenant : public Boite{
+  public :
+    // Attributs :
+    float _chargeMax; 
+    std::vector<Carton*> _cartons;
+    
+    // Méthodes génériques
+    // Contructeur
+    Contenant(int x, int y, int z, const int dimX, const  int dimY, const int dimZ, float chargeMax);
+    // Destructeur
+    ~Contenant(void) {}
+    
+    // Gestion des cartons
   
 
 };
 
 
 #endif
+
+/* REMARQUES :
+1 - Jacques
+  Je ne me souviens pas de ce que l'attribut chargeMax représente
+
+*/
