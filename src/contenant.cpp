44b1474d5653186cd6ea1589db1924d6751674b1
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-15 15:01:10 +0200

    Update contenant.cpp

--- src/contenant.cpp
+++ src/contenant.cpp
@@ -1,7 +1,15 @@
 #include "contenant.h"
 
-Contenant::Contenant(int x=0, int y=0, int z=0, int dimX,  int dimY, int dimZ, const std::string name, float chargeMax){
-  Boite(x,y,z,dimX,dimY,dimZ, name);
-  _chargeMax=chargeMax;
+Contenant::Contenant(int x, int y, int z, int dimX,  int dimY, int dimZ, const std::string name, float chargeMax)
+: Boite(x,y,z,dimX,dimY,dimZ, name), _chargeMax=chargeMax
+{
+  //Attribut qui sera intancié plus tard
   _cartons=NULL;
 }
+
+/*
+1 - Jacques
+  Il faudrait faire un traitement des coordonnées du contenant. 
+  J'ai peur qu'on confonde deux contenants si leurs coordonnées sont toutes à 0.
+
+*/
