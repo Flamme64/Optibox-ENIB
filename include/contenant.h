9788c3d457c7653c328a64b9c0116d331448e2fd
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-19 14:54:14 +0200

    Update contenant.h

--- include/contenant.h
+++ include/contenant.h
@@ -4,12 +4,12 @@
 #include <vector>
 
 #include "pave.h"
-class Carton; //Il faudra mettre un include dans le fichier final
+class Carton;
 
 class Contenant : public Pave{
   public :
     // Attributs :
-    float _masseMax; 
+    float _masseMax; // La masse maximum qu'un contenant peut supporter
     std::vector<Carton*> _cartons;
     
     // Méthodes génériques
@@ -17,6 +17,11 @@ class Contenant : public Pave{
     Contenant(int x, int y, int z, int dimX, int dimY, int dimZ, const std::string name, float masseMax) : Pave(x,y,z,dimX,dimY,dimZ, name), _masseMax(masseMax) {}
     // Destructeur
     ~Contenant(void) {}
+    
+    // Mutateurs de sécurité :
+      void setDimX(int dimX) {print("ERROR : Modification de la dimX d'un contenant");}
+      void setDimY(int dimY) {print("ERROR : Modification de la dimY d'un contenant"):}
+      void setDimZ(int dimZ) {print("ERROR : Modification de la dimZ d'un contenant");}
     
     // Gestion des cartons :
     
@@ -28,8 +33,7 @@ class Contenant : public Pave{
 #endif
 
 /* REMARQUES :
-1 - Jacques
-  Je ne me souviens pas de ce que l'attribut chargeMax représente
+
 
 
 */
