c89818646ebf8b23de94e133e86d1221868548c3
norbert1453 <n3parker@enib.fr>
2016-04-19 15:04:25 +0200

    Update rangement.h

--- include/rangement.h
+++ include/rangement.h
@@ -6,19 +6,24 @@
 #include "contenant.h"
 #include "carton.h"
 #include "espaceLibre.h"
-;
+
 class Rangement{
   public :
   // Attributs :
+    int qContenant;
     std::vector<Carton*> _cartRang;
     std::vector<Contenant*> _contRang;
+    std::vector<EspaceLibre*> _espLib;
   
   // Méthodes génériques :
   // Constructeur
-  Rangement(void) {}
+  Rangement(void) qContenant(0) :{}
   // Destructeur
   ~Rangement(void) {}
   
+  // Vérification de la possibilité de placement des cartons
+  verifMasseLegale(Carton * carton);
+  verifPlaceDispo(Carton * carton, int x, int y, int z);
 };
 
 #endif
