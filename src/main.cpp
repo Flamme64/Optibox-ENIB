6336612c466eed814428fb67201db78f2d4a2ad7
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-22 17:00:35 +0200

    Fusion avec Norbert

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main(){
   
 
 
-	std::cout << "Ceci est un test de master" << std::endl ;
-	system("PAUSE");
+	std::cout << "Ceci est un test d'affichage sous Visual Studio" << std::endl ;
+ 	system("PAUSE");
 	return 0;
 }
