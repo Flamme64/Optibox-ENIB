629ce31da097d6d701effc1e3b9542e025020fd1
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-22 17:00:11 +0200

    Fusion avec Norbert

--- src/contenant.cpp
+++ src/contenant.cpp
@@ -3,8 +3,10 @@
 
 
 /*
-1 - Jacques
+1 - Jacques (RESOLU)
   Il faudrait faire un traitement des coordonnées du contenant. 
   J'ai peur qu'on confonde deux contenants si leurs coordonnées sont toutes à 0.
+  
+  On les traite séparément donc pas de confusion.
 
 */
