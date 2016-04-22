093ea892b294fdd7c408a0d4bacbce0a9ab24d25
DarkFyer1 <jacky.vergine@gmail.com>
2016-04-22 16:57:58 +0200

    Fusion avec Norbert

--- include/pave.h
+++ include/pave.h
@@ -16,20 +16,20 @@ class Pave{
     
     //Méthodes génériques :
       //Constructeur :
-      Pave(int x, int y, int z, int dimX, int dimY, int dimZ, const std::string name) : _dimX(dimX), _dimY(dimY), _dimZ(dimZ), _name(name), _point(x,y,z) {}
+	Pave(int x, int y, int z, int dimX, int dimY, int dimZ, const std::string name) : _dimX(dimX), _dimY(dimY), _dimZ(dimZ), _name(name), _point(x,y,z) {}
       // Destructeur
-      ~Pave(void) {}
+	~Pave(void) {}
       
       // Accesseurs
-      int getDimX(void) {return _dimX;}
-      int getDimY(void) {return _dimY;}
-      int getDimZ(void) {return _dimZ;}
-      std::string getName(void) {return _name;}
+	int getDimX(void) {return _dimX;}
+	int getDimY(void) {return _dimY;}
+	int getDimZ(void) {return _dimZ;}
+	std::string getName(void) {return _name;}
       
       // Mutateurs
-      virtual void setDimX(int dimX) {_dimX=dimX;}
-      virtual void setDimY(int dimY) {_dimY=dimY;}
-      virtual void setDimZ(int dimZ) {_dimZ=dimZ;}
+	virtual void setDimX(int dimX) {_dimX=dimX;}
+	virtual void setDimY(int dimY) {_dimY=dimY;}
+	virtual void setDimZ(int dimZ) {_dimZ=dimZ;}
       
     
   
